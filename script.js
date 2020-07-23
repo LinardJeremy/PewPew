37c9e32d5601eccbf3d9a4598a03fb4b68d9e870
Linard Jeremy <linard.jeremy.5@gmail.com>
2020-07-23 16:45:42 +0200

    fin de jornée

---
+++ script.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById("background");
+var context = canvas.getContext("2d");
+
+context.fillStyle = "gray";
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+context.fillStyle = "red";
+context.fillRect(5, 5, 10, 15);
+context.fillStyle = "blue";
+context.fillRect(25, 25, 20, 20);
+context.fillStyle = "green";
+context.fillRect(50, 50, 20, 40);
