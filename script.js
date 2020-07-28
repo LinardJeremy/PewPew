73a8d15c0958a94c1ba1ca66cefe7a1ffb06c0ff
Linard Jeremy <linard.jeremy.5@gmail.com>
2020-07-28 10:19:16 +0200

    immobilisation de l'ennemiµ

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let x = 50;
 let y = 450;
 let my= 400;
 let mx;
-let dx = 2;
+let dx = 4;
  let dy = -2;
  let xx =2;
 let rightPressed = false;
@@ -19,8 +19,13 @@ let spacePressed = false;
 let randomX = Math.random()*450;
 let randomY = Math.random()*400;
 let touch = false;
+let interval = setInterval(drawMissile,15);
 
-document.getElementById('start').addEventListener('click',function start(){
+// document.getElementById('start').addEventListener('click',start());
+
+
+ennemies();
+setInterval(ennemies, 10);
 function drawRect() {
 ctx.beginPath();
  ctx.rect(x, y, 50, 50);
@@ -32,15 +37,15 @@ function draw() {
   ctx.clearRect(0,0,canvas.width,canvas.height);
   drawRect();
   if(rightPressed) {
-    // x += dx;
-    x += 2;
+     x += dx;
+    
     if (x + dx > 450){
       x = 450;
   }
 }
 else if(leftPressed) {
     // x += -dx;
-    x +=-2;
+    x +=-dx;
     if (x < 0){
       x = 0;
   }  
@@ -57,26 +62,26 @@ ctx.rect(mx,my, 20, 20);
 
 }
 function drawMissile() {
-  //  ctx.clearRect(mx,(my+dy),50,50);
+    ctx.clearRect(mx,(my+dy),50,50);
      missile();
       my += dy;
   }
  function ennemies(){
  ctx.beginPath();
-  ctx.rect(randomX, randomY, 55, 55);
+ ctx.rect(randomX, randomY, 55, 55);
   ctx.fillStyle = "green";
   ctx.fill();
   ctx.closePath();
  }
- function drawEnnemies(){
-  // ctx.clearRect(0,randomY, canvas.width,canvas.height);
-   ennemies();
-    randomX += xx;
-   if(randomX + xx > (canvas.width-50) || randomX+ xx < 0) {
-    xx = -xx;
- }
- }
-  let myVar =setInterval(drawEnnemies,5);
+//  function drawEnnemies(){
+//   // ctx.clearRect(0,randomY, canvas.width,canvas.height);
+//    ennemies();
+//     randomX += xx;
+//    if(randomX + xx > (canvas.width-50) || randomX+ xx < 0) {
+//     xx = -xx;
+//  }
+//  }
+  // let myVar =setInterval(drawEnnemies,5);
 
 setInterval(draw,30);
 
@@ -126,8 +131,8 @@ if (x < randomX+ 55 &&
     console.log("touche");
     score.innerHTML= s;
     if (touch===true){
-    clearTimeout(myVar);
-     setTimeout(myVar,100);
+    // clearTimeout(myVar);
+    //  setTimeout(myVar,100);
     touch = false;
     s++;
     }
@@ -139,6 +144,4 @@ document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("keydown", spaceShoot, false);
 document.addEventListener("keyup", spaceNot, false);
-let interval = setInterval(drawMissile,15);
 
-});
